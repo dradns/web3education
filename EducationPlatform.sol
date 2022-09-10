2c48507a745bb4a9e11bdfd269e19eb0aff2fa1c
Nikita Savin <42571773+dradns@users.noreply.github.com>
2022-09-10 16:55:24 +0300

    Update EducationPlatform.sol

--- EducationPlatform.sol
+++ EducationPlatform.sol
@@ -30,7 +30,7 @@ contract EducationPlatform2 {
         uint courseId;
         address userId;
         bool isSubmitted;
-        string submission;
+        string comment;
         bool isChecked;
     }
 
@@ -47,7 +47,7 @@ contract EducationPlatform2 {
         return userCourses.length;
     }
 
-    //get all my registered courses as a user
+    //get all registered courses for current user
     function getAllMyRegisteredCourses() public view returns (UserToCourse[] memory){
         uint counter = 0;
         uint j = 0;
@@ -66,22 +66,92 @@ contract EducationPlatform2 {
         return temp;
     }
 
-    // function _createZombie(string memory _name, uint _dna) private {
-    //     uint id = zombies.push(Zombie(_name, _dna)) - 1;
-    //     zombieToOwner[id] = msg.sender;
-    //     ownerZombieCount[msg.sender]++;
-    //     emit NewZombie(id, _name, _dna);
-    // }
+    //course submission from user
+    function submitCourse(uint _courseId, string memory _comment) public returns (bool){
+        for (uint i = 0; i < userCourses.length; i++){
+            if (userCourses[i].userId == msg.sender && _courseId == userCourses[i].courseId){
+                userCourses[i].isSubmitted = true;
+                userCourses[i].comment = _comment;
+                _increaseUserBalance(20);
+                return true;
+            }
+        }
+        return false;
+    }
 
     //общий список всех пользователей
     User[] public users;
 
+    //become a member in user array
+    function registrationOnEducationPlatform() public{
+        users.push(User(msg.sender,0,false));
+    }
+
+    //increase user balance
+    function _increaseUserBalance(uint _ammount) private{
+        for (uint i = 0; i < users.length; i++){
+            if (users[i].userId == msg.sender){
+                users[i].balance += _ammount;
+            }
+        }
+    }
+
+    //become review
+    function becomeRewier() public{
+        for (uint i = 0; i < users.length; i++){
+            if (users[i].userId == msg.sender){
+                users[i].isRewier = true;
+            }
+        }
+    }
+
+    //get all courses for review for current user
+    function getAvailForRewier() public view returns (UserToCourse[] memory){
+        uint counter = 0;
+        uint j = 0;
+        for (uint i = 0; i < userCourses.length; i++){
+            if (userCourses[i].userId != msg.sender && userCourses[i].isSubmitted == true && userCourses[i].isChecked == false){
+                counter++;
+            }
+        }
+
+        UserToCourse[] memory tempForReview = new UserToCourse[](counter);
+
+        for (uint i = 0; i < userCourses.length; i++){
+            if (userCourses[i].userId != msg.sender && userCourses[i].isSubmitted == true && userCourses[i].isChecked == false){
+                tempForReview[j] = userCourses[i];
+                j++;
+            }
+        }
+        return tempForReview;
+    }
+
+    //set specific course as checked
+    function setAsChecked(uint _courseId, address _userAddress, bool _result) public returns (bool){
+        for (uint i = 0; i < userCourses.length; i++){
+            if (userCourses[i].userId != msg.sender && userCourses[i].courseId == _courseId && userCourses[i].userId == _userAddress){
+                userCourses[i].isChecked = _result;
+                _increaseUserBalance(50);
+            }
+        }
+        return true;
+    }
+
+    //get current user
+    function getUser() public view returns (User memory){
+        User memory temp; 
+        for (uint i = 0; i < users.length; i++){
+            if (users[i].userId == msg.sender){
+                temp = users[i];
+            }
+        }
+        return temp;
+    }
+
     //array of all avaialiable courses 
     Course[] public courses;
 
-    /**
-     * заполнение списка курсов 
-     */
+    //feel courses list on the platform 
     function feelCourseList() public {
         courses.push(Course(1, "testName1", "testDescription1", 1, "someImage1"));
         courses.push(Course(2, "testName2", "testDescription2", 2, "someImage2"));
@@ -91,68 +161,16 @@ contract EducationPlatform2 {
         courses.push(Course(6, "testName6", "testDescription6", 2, "someImage6"));
         courses.push(Course(7, "testName7", "testDescription7", 2, "someImage7"));
         courses.push(Course(8, "testName8", "testDescription8", 2, "someImage8"));
-        courses.push(Course(8, "testName9", "testDescription9", 2, "someImage9"));
+        courses.push(Course(9, "testName9", "testDescription9", 2, "someImage9"));
     }
 
+    //length of platform courses
     function getCoursesLength() public view returns (uint){
         return courses.length;
     }
 
+    //specific course on platform
     function getSpecificCourseById(uint _index) public view returns (Course memory){
         return courses[_index];
     }
-
-    /**
-     * регистрация юзера на курс
-     */
-    // function registrationToCourse(uint _courseId) public {
-    //     User.courseIds.push(_courseId);
-    // }
-
-    /**
-     * получение массива айди_курсов пользователя
-     */
-    // function getMyUserCourses() view public returns (uint){
-    //     //вернуть список всех идентификатор пользака
-    //     return userCourses[msg.sender];
-    // }
-
-    /**
-     * function for reviewers
-     * function check is there availiable courses for checking
-     * return array of avaialible courses
-     */
-
-    // function recieveAvailCourses(address _chekerAddress) public {
-    //     //логика получения доступных курсов для проверки
-    //     return uint[];
-    // }
-
-    /**
-     * function for reviewers
-     * функция получает на вход курc_на_пользака и bool result
-     * функция меняет статус курса на тру или false
-     */
-
-    // function courceCheck(uint _courseId, bool result) public returns (bool){
-    //     //return bool result
-    //     return result;
-    // }
-
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
-    // function retrieve() public view returns (uint256){
-    //     return number;
-    // }
-    //декларируем тип ивента
-    //event IntegersAdded(uint x, uint y, uint result);
-    //эмитим ивент
-    //emit IntegersAdded(_x, _y, result);
-
-    // mapping (uint => address) public zombieToOwner;
-    // mapping (address => uint) ownerZombieCount;
-
-
 }
